493415cba0f364925ffd5f8ad18418abe0b702ba
Marcelo Freitas <marcelofreitasx@gmail.com>
2024-10-12 15:41:37 -0300

    fix: product usecase and repository tests

--- test/unit/domain/repositories/products_test.dart
+++ test/unit/domain/repositories/products_test.dart
@@ -1,4 +1,5 @@
 import 'package:diletta_challenge/domain/entities/entities.dart';
+import 'package:diletta_challenge/domain/params/options.dart';
 import 'package:diletta_challenge/domain/repositories/repositories.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -9,29 +10,30 @@ import '../../mocks.dart';
 void main() {
   group('ProductRepository', () {
     late ProductRepository sut;
+    final OptionsParams options = OptionsParams();
 
     setUp(() {
       sut = MockProductRepository();
     });
 
     test('should return a list of products entity', () async {
-      when(() => sut.getProducts()).thenAnswer(
+      when(() => sut.getProducts(options)).thenAnswer(
         (_) async => [
           ProductEntity(id: faker.randomGenerator.integer(1000000), title: faker.company.name()),
           ProductEntity(id: faker.randomGenerator.integer(1000000), title: faker.company.name()),
         ],
       );
 
-      final entity = await sut.getProducts();
+      final entity = await sut.getProducts(options);
 
       expect(entity, isA<List<ProductEntity>>());
       expect(entity.length, 2);
     });
 
     test('should throw an exception when usecase throws an exception', () async {
-      when(() => sut.getProducts()).thenThrow(Exception('Error fetching products'));
+      when(() => sut.getProducts(options)).thenThrow(Exception('Error fetching products'));
 
-      expect(() => sut.getProducts(), throwsException);
+      expect(() => sut.getProducts(options), throwsException);
     });
   });
 }
--- test/unit/domain/usecases/products_list_test.dart
+++ test/unit/domain/usecases/products_list_test.dart
@@ -1,4 +1,5 @@
 import 'package:diletta_challenge/domain/entities/entities.dart';
+import 'package:diletta_challenge/domain/params/params.dart';
 import 'package:diletta_challenge/domain/usecases/usecases.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -9,29 +10,30 @@ import '../../mocks.dart';
 void main() {
   group('GetProductsListUseCase', () {
     late GetProductsListUseCase sut;
+    final OptionsParams options = OptionsParams();
 
     setUp(() {
       sut = MockGetProductsListUseCase();
     });
 
     test('should return a list of products entity', () async {
-      when(() => sut.execute()).thenAnswer(
+      when(() => sut.execute(options)).thenAnswer(
         (_) async => [
           ProductEntity(id: faker.randomGenerator.integer(1000000), title: faker.company.name()),
           ProductEntity(id: faker.randomGenerator.integer(1000000), title: faker.company.name()),
         ],
       );
 
-      final entity = await sut.execute();
+      final entity = await sut.execute(options);
 
       expect(entity, isA<List<ProductEntity>>());
       expect(entity.length, 2);
     });
 
     test('should throw an exception when usecase throws an exception', () async {
-      when(() => sut.execute()).thenThrow(Exception('Error fetching products list'));
+      when(() => sut.execute(options)).thenThrow(Exception('Error fetching products list'));
 
-      expect(() => sut.execute(), throwsException);
+      expect(() => sut.execute(options), throwsException);
     });
   });
 }
